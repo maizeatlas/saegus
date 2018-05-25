1e8f02fa6bec2c893709c11675a2293e40bdbe7c
jjdoc <jjdoc@udel.edu>
2018-05-25 16:07:43 -0400

    Shell script has been refined. Some header text has been added to tell let the user know the input parameters at the time the script is run. First parameter is the "run id" followed by the number of replicates. i.e. bash simulated_mlm.sh small 10
    More interesting is that TASSEL will not process the runs where the
    entries of the population structure vector are not large enough. The
    error is "fixed effects design matrix does not have complete rank."
    I will increase the sample size before proceeding with the actual data.

--- src/docs/user_guide/simulated_mlm.sh
+++ src/docs/user_guide/simulated_mlm.sh
@@ -1,7 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-for $filename in $@;
+
+echo "Run ID: $1, Number of Replicates $2"
+run_id=$1
+number_of_replicates=$2
+final_rep_index="$((number_of_replicates - 1))"
+
+echo "Beginning TASSEL analysis of Run ID: $run_id"
+echo "Number of Replicates: $number_of_replicates"
+echo "First configuration file: small_0_gwas_pipeline.xml"onca
+echo "Final configuration file: small_"$final_rep_index"_gwas_pipeline.xml"
+
+for i in `seq 0 $final_rep_index`
 do
-   echo "Running $filename now.\n"
-   ./run_pipeline.pl -Xmx6g -configFile $filename
+    config_file_name=$run_id$i"_gwas_pipeline.xml"
+    echo "$config_file_name"
+    ./run_pipeline.pl -Xmx6g -configFile $config_file_name
 done
