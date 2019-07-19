c074882692d5aca55c0c6fad449bb0afa4de6628
Maize ATLAS <maizeatlas@gmail.com>
2019-07-19 16:49:10 -0400

    fixed formula for calculate_error_variance
    fixed formula for calculate_error_variance, given h2, Vg and number of seg sites

--- src/saegus/operators.py
+++ src/saegus/operators.py
@@ -492,7 +492,9 @@ def calculate_error_variance(pop, heritability):
     """
     assert 0 < heritability < 1, "heritability must be between 0 anc 1"
     variance_of_g = np.var(pop.indInfo('g'))
-    epsilon = variance_of_g*(1/heritability - 1)
+    # epsilon = (variance_of_g-(self.heritability*variance_of_g))/self.heritability
+    # distribute across segregating sites
+    epsilon = ((variance_of_g-(heritability*variance_of_g)))/heritability/len(pop.dvars().segSites)
     pop.dvars().epsilon = epsilon
 
 # todo Create documentation entry for calculate_p
